07d76346daee955ee86413becd1c58d41084077c
liuhuihui <244701374@qq.com>
2023-02-26 23:05:51 +0800

    init

--- .umirc.ts
+++ .umirc.ts
@@ -1,36 +1,12 @@
 export default {
   npmClient: "yarn",
-  routes: [
-    { path: "/", component: "@/pages/index" },
-    { path: "/transformObj", component: "@/pages/transformObj" },
-    { path: "/camera", component: "@/pages/camera" },
-    { path: "/orbitControls", component: "@/pages/OrbitControls" },
-    { path: "/geometry", component: "@/pages/geometry" },
-    { path: "/ligui", component: "@/pages/ligui" },
-    { path: "/texture", component: "@/pages/texture" },
-    { path: "/materials", component: "@/pages/materials" },
-    { path: "/standardMaterial", component: "@/pages/meshStandardMaterial" },
-    { path: "/fontLoader", component: "@/pages/fontLoader" },
-    //
-    { path: "/light", component: "@/pages/lights/light" },
-    { path: "/lightAnim", component: "@/pages/lights/lightAnim" },
-    { path: "/shadowBase", component: "@/pages/lights/shadows_base" },
-    { path: "/shadow", component: "@/pages/lights/shadows" },
-    { path: "/shadowsBaking", component: "@/pages/lights/shadowsBaking" },
-
-    // DEMO
-    { path: "/3DText", component: "@/pages/demos/3DText" },
-    { path: "/dlight", component: "@/pages/lights/DLights" },
-    { path: "/hauntedHouse", component: "@/pages/demos/HauntedHouse" },
-
-    // http://www.webgl3d.cn/Three.js/?_blank
-    { path: "/gdemo", component: "@/pages/1_demo/index" },
-    { path: "/geometry", component: "@/pages/geometry/index" },
-    { path: "/material", component: "@/pages/material/index" },
-    { path: "/video", component: "@/pages/videoStu/index" },
-  ],
+  // 设置hash地址
+  history: {
+    type: "hash",
+  },
+  routes: [{ path: "/", component: "@/pages/videotape" }],
   // publicPath: "/public/",
-  publicPath: "/",
+  publicPath: "./",
 };
 
 /**
