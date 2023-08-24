80a23479ad66bf9eff97bc10826fa7065745b339
liuhuihui <244701374@qq.com>
2023-08-24 14:55:01 +0800

    del

--- src/pages/demos/maps/fragment.glsl
+++
@@ -1,9 +0,0 @@
-uniform float time;
-uniform vec3 colorA; 
-uniform vec3 colorB;   
-varying vec2 vUv;   
-
-void main() {  
-  vec3 color = vUv.x < time ? colorB : colorA; 
-  gl_FragColor = vec4(color,1.0);
-}
